c068b72bc42e67ec4b1ec7ab82fcdd753b72357d
Cygnan <mail@cygnan.com>
2017-06-22 01:00:57 +0900

    Fixed typo

--- reboot.sh
+++ reboot.sh
@@ -12,6 +12,6 @@ id=`echo -n ${res} | sed -e 's/^.*sWebSessionid\ value=\([0-9\-]*\)>.*$/\1/'`
 [ "${num}" != "`echo -n ${num} | sed -e 's/\([^0-9\-]\)//g'`" -o "${num}" = '' ] && echo 'Failed...' && continue
 data="reboot=%3d%22%e5%86%8d%e8%b5%b7%e5%8b%95&sWebSessionnum=${num}&sWebSessionid=${id}"
 dataLen=`echo -n ${data} | wc -c`
-echo -ne "POST /cgi-bin/cgi?req=inp&res=waiting_page.html HTTP/1.0\r\nHost: ${routerIp}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: ${dataLen}\r\nAuthorization: Basic ${secret}\r\n\r\n${data}" | nc ${routerIp} 80 > /dev/null && echo 'Successfully sent HTTP POST request.' && break || echo 'Failed...'
+echo -ne "POST /cgi-bin/cgi?req=inp&res=waiting_page.html HTTP/1.0\r\nHost: ${routerIp}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: ${dataLen}\r\nAuthorization: Basic ${secret}\r\n\r\n${data}" | nc ${routerIp} 80 > /dev/null && echo 'Successfully sent a HTTP POST request.' && break || echo 'Failed...'
 sleep 3
 done
